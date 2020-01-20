1fe7c8158486a6fd04a15051ccff66bb87b0656e
Nicolas <npradenas.vargas@gmail.com>
2020-01-20 09:31:35 -0300

    Acotar plot de la reflectancia
    -script que acota reflectancia
    - la vista incluye una opcion para acotar

---
+++ acotarReflectancia.m
@@ -0,0 +1,12 @@
+
+function [new_reflectancia,valores] = acotarReflectancia(reflectancia, limiteIzquierdo, limiteDerecho)
+	disp(limiteIzquierdo)
+	disp(limiteDerecho)
+	valores = [limiteIzquierdo:limiteDerecho];
+	new_reflectancia = zeros(limiteDerecho-limiteIzquierdo,1);
+	indice = 1;
+	for posReflectancia = limiteIzquierdo:limiteDerecho
+		new_reflectancia(indice) = reflectancia(posReflectancia-349);
+		indice = indice+1;
+	end
+end
