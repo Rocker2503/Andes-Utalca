9d504110df5b019eb6c3e9c8295f25c88124eae7
Juan Ignacio <jcorderuiz@gmail.com>
2020-01-30 16:53:10 -0300

    agregar indice vista NxM
    modal agregar indice para cada hashmap NxM

--- exportarIndicesNxM.m
+++
@@ -1,9 +0,0 @@
-function [] = exportarIndicesNuevo(fileNameIndice, indicesMap)
-	    fid= fopen(fileNameIndice,'w');
-        
-        keySet = keys(indicesMap);
-        for i = 1:length(indicesMap)           
-            fprintf(fid, '%s;%f\n', keySet{i}, indicesMap(keySet{i}) );
-        end
-        fclose(fid);
-end
